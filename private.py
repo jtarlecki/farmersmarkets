2b96c00752afd41ab124a0aa3a525d90436a7278
jtarlecki <jtarlecki@yahoo.com>
2014-06-18 19:08:43 -0400

    remove private

--- private.py
+++
@@ -1,7 +0,0 @@
-class Private(object):
-    
-    def __init__(self):
-        self.host = 'jaytarlecki.com'
-        self.dbname = 'jaytarle_farmersmarkets'
-        self.user = 'jaytarle_admin'
-        self.password = 'str8edge'
